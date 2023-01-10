adade0fe3acf143e35d82272d6f9330099edaaed
Tseng <tseng7510@gmail.com>
2023-01-10 17:58:53 +0800

    feat: 新增/修改功能(#4) 修改style.css權重問題 btn.html 頁面icon名稱更新deep > dark form.html 頁面密碼input欄位增加眼睛按鈕，可切換是否顯示密碼 main.js 增加密碼input欄位相關設定

--- js/main.js
+++ js/main.js
@@ -1677,6 +1677,39 @@ function langFunction(obj) {
       }
     });
   }
+
+  // form password eyes
+  let checkEye = document.querySelectorAll('.passwordEye');
+  checkEye.forEach((item) => {
+    let passwordInput = item.parentNode.querySelector('[type="password"]');
+    item.addEventListener('click', function (e) {
+      if (e.target.classList.contains('hide')) {
+        //換class / type
+        e.target.classList.remove('hide');
+        e.target.classList.add('show');
+        if (webLang) {
+          obj.password.data.forEach((s) => {
+            if (webLang.slice(0, 2) == s.lang) {
+              e.target.innerHTML = s.hide;
+            }
+          });
+        }
+        passwordInput.setAttribute('type', 'text');
+      } else {
+        passwordInput.setAttribute('type', 'password');
+        e.target.classList.remove('show');
+        e.target.classList.add('hide');
+        e.target.innerHTML = obj.show;
+        if (webLang) {
+          obj.password.data.forEach((s) => {
+            if (webLang.slice(0, 2) == s.lang) {
+              e.target.innerHTML = s.show;
+            }
+          });
+        }
+      }
+    });
+  });
 }
 
 langFunction({
@@ -1718,5 +1751,20 @@ langFunction({
     ],
     //預設語系
     default: 'Search',
+  },
+  password: {
+    data: [
+      {
+        lang: 'zh',
+        show: '顯示密碼',
+        hide: '隱藏密碼',
+      },
+      //...由此新增其他語系
+    ],
+    //預設語系
+    default: {
+      show: 'show',
+      hide: 'hide',
+    },
   },
 });
